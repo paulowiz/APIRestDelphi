f0ff42671ec822396d2ebbf78f94ace2211fae81
Paulo Mota <paaulowiz@gmail.com>
2020-03-22 23:30:45 -0300

    Implementation endpoit to test 'users'

--- horse_api.dpr
+++ horse_api.dpr
@@ -6,6 +6,8 @@ program horse_api;
 
 uses
   Horse,
+  Horse.Jhonson,
+  System.JSON,
   ServerReact.Model.Connection in 'model\ServerReact.Model.Connection.pas',
   ServerReact.Model.Entidades.User in 'model\Entidades\ServerReact.Model.Entidades.User.pas',
   ServerReact.Model.DAOGeneric in 'model\ServerReact.Model.DAOGeneric.pas';
@@ -16,6 +18,8 @@ var
 begin
   App := THorse.Create(9000);
 
+  App.Use(Jhonson);
+
   App.Get('/ping',
     procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
     begin
@@ -24,5 +28,13 @@ begin
       Res.Send('pong');
     end);
 
+    App.Get('/users',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      FDAO : iDAOGeneric<TUSER>;
+    begin
+      FDAO := TDAOGeneric<TUSER>.New;
+      Res.Send<TJsonArray>(FDAO.Find);
+    end);
   App.Start;
 end.
